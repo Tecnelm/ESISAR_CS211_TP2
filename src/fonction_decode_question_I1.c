9af230879850f832ad464667ac685328e93871ed
Tecnelm <cleman91@gmail.com>
2019-11-02 17:16:30 +0100

    question 1done

--- src/fonction_decode_question_I1.c
+++ src/fonction_decode_question_I1.c
@@ -9,22 +9,22 @@ void decode_fonction(char* source_path,char* transporteur_path)
     FILE *source;
     char character;
     int counter = 0;
-    char buffer=0;
+    char buffer=0; // creation of a buffer to write in the source byte by byte
 
     transporteur = openFile(transporteur_path,"r");
     source = openFile(source_path,"w");
 
     do {
-        character = (char)fgetc(transporteur);
-        if(is_alpha(character))
+        character = (char)fgetc(transporteur); // get the next char of transporteur
+        if(is_alpha(character)) // check if is alphanum
         {
-            if (counter == 8){
+            if (counter == 8){ // if the buffer is full write it in the source folder
                 counter = 0;
-                //print_binary_char(buffer);
                 fputc(buffer,source);
                 buffer = 0;
 
             }
+            // add the right bit to the buffer
             if(is_upper(character)){
                 buffer = (char)(buffer << 1);
                 buffer++;
